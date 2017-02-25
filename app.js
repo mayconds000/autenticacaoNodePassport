ea3bb27b27ff14682f62471e313151a9301f519a
mayconds000 <mayconds000@gmail.com>
2017-02-25 16:17:42 -0300

    adicionado autenticação e pagina de login

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy;
+
+var User = require('./models/user');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -16,6 +19,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -24,8 +28,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Passport
 app.use(passport.initialize());
 app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 mongoose.connect('mongodb://localhost:27017/passport_local', function(err) {
   if (err) {
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
+
+var passport = require('passport');
 var express = require('express');
 var router = express.Router();
 var User = require('./../models/user');
 
-/* GET users listing. */
+// Registes
 router.get('/', function(req, res, next) {
   res.render('register', {})
 });
@@ -14,8 +16,28 @@ router.post('/', function(req, res) {
       return;
     } 
 
-    res.redirect('/login');
+    res.redirect('/users/login');
   })
+});
+
+//login
+router.get('/login', function(req, res) {
+  res.render('login', {});
+});
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+  res.redirect('/');
+});
+
+//Delete
+router.post('/:id', function(req, res) {
+  User.findByIdAndRemove(req.params.id, function(err, deleted) {
+   if(err) {
+     console.warn(err);
+     return;
+   }
+    res.redirect('/users');
+  });
 });
 
 module.exports = router;
